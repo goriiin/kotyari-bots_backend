c22806d53f6820b35df3ab146f3572e9aa58bde7
goriiin <afk00.17@bk.ru>
2025-09-10 22:59:57 +0300

    bot-2: add bots service

---
+++ internal/delivery/bots/delete.go
@@ -0,0 +1,14 @@
+package bots
+
+import (
+	"context"
+	gen "github.com/goriiin/kotyari-bots_backend/internal/gen/bots"
+)
+
+func (h *Handler) DeleteBotById(ctx context.Context, params gen.DeleteBotByIdParams) (gen.DeleteBotByIdRes, error) {
+	err := h.u.Delete(ctx, params.BotId)
+	if err != nil {
+		return nil, err
+	}
+	return &gen.NoContent{}, nil
+}
