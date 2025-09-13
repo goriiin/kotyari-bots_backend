66bbddf9e72f5d9bf68ee7ca05f42dd666c59a00
goriiin <afk00.17@bk.ru>
2025-09-14 00:09:40 +0300

    BOT-2: format / lint

--- internal/repo/bots/create.go
+++ internal/repo/bots/create.go
@@ -6,8 +6,8 @@ import (
 	"github.com/goriiin/kotyari-bots_backend/internal/model"
 )
 
-func (r *PGRepo) Create(ctx context.Context, b model.Bot) error {
-	_, err := r.pool.Exec(ctx,
+func (r *BotsRepository) Create(ctx context.Context, b model.Bot) error {
+	_, err := r.db.Exec(ctx,
 		`insert into bots (id, bot_name, system_prompt, moderation_required, profile_ids) values ($1, $2, $3, $4)`,
 		b.ID, b.Name, b.SystemPrompt, b.ModerationRequired, b.ProfileIDs)
 	return err
--- internal/repo/bots/list.go
+++ internal/repo/bots/list.go
@@ -6,8 +6,8 @@ import (
 	"github.com/goriiin/kotyari-bots_backend/internal/model"
 )
 
-func (r *PGRepo) List(ctx context.Context) ([]model.Bot, error) {
-	rows, err := r.pool.Query(ctx, `select id, bot_name, system_prompt from bots order by created_at desc`)
+func (r *BotsRepository) List(ctx context.Context) ([]model.Bot, error) {
+	rows, err := r.db.Query(ctx, `select id, bot_name, system_prompt from bots order by created_at desc`)
 	if err != nil {
 		return nil, err
 	}
@@ -16,7 +16,7 @@ func (r *PGRepo) List(ctx context.Context) ([]model.Bot, error) {
 	var res []model.Bot
 	for rows.Next() {
 		var b model.Bot
-		if err = rows.Scan(&b.ID, &b.Name, &b.SystemPrompt); err != nil {
+		if err := rows.Scan(&b.ID, &b.Name, &b.SystemPrompt); err != nil {
 			return nil, err
 		}
 		res = append(res, b)
