b3d6ba3e51e6ade3c2b5dee92bf8a2f960237921
goriiin <afk00.17@bk.ru>
2025-09-15 14:53:01 +0300

    BOT-2: add migrate

--- cmd/bots/migrate/main.go
+++ cmd/bots/migrate/main.go
@@ -1,17 +1,10 @@
-// path: /Users/dkoshenkov/Desktop/kotyari-bots_backend/cmd/bots/migrate/main.go
 package main
 
 import (
-	"flag"
-	"log"
-	"strings"
-
-	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/pgx/v5"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/goriiin/kotyari-bots_backend/internal/apps/bots"
 	"github.com/goriiin/kotyari-bots_backend/pkg/config"
-	"github.com/goriiin/kotyari-bots_backend/pkg/postgres"
+	"github.com/goriiin/kotyari-bots_backend/pkg/migrate"
+	"log"
 )
 
 var Config = "bots-local"
@@ -22,41 +15,5 @@ func main() {
 		log.Fatalf("config load: %v", err)
 	}
 
-	dsn := postgres.ToUrl(cfg.Database)
-	if !strings.HasPrefix(dsn, "pgx5://") {
-		dsn = "pgx5://" + strings.TrimPrefix(dsn, "postgres://")
-	}
-
-	log.Printf("Using DSN for migration: %s", dsn)
-
-	m, err := migrate.New(
-		"file:///app/migrations",
-		dsn,
-	)
-	if err != nil {
-		log.Fatalf("failed to create migrate instance: %v", err)
-	}
-
-	args := flag.Args()
-	if len(args) < 1 {
-		log.Fatal("usage: migrate <up|down>")
-	}
-
-	command := args[0]
-	switch command {
-	case "up":
-		log.Println("Applying migrations up...")
-		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-			log.Fatalf("failed to apply migrations up: %v", err)
-		}
-		log.Println("Migrations applied successfully")
-	case "down":
-		log.Println("Applying migrations down...")
-		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
-			log.Fatalf("failed to apply migrations down: %v", err)
-		}
-		log.Println("Migrations rolled back successfully")
-	default:
-		log.Fatalf("unknown command: %s", command)
-	}
+	migrate.Run(cfg.Database)
 }
