9abb834b2306ba6504e3f62bd7a44355404e9b99
goriiin <afk00.17@bk.ru>
2025-11-01 16:29:51 +0300

    notask: cors

--- pkg/cors/cors.go
+++ pkg/cors/cors.go
@@ -4,7 +4,10 @@ import "github.com/rs/cors"
 
 func New() *cors.Cors {
 	return cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedOrigins: []string{
+			"http://localhost:3000",
+			"http://83.166.253.130:3000",
+		},
 		AllowCredentials: true,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Authorization", "Content-Type"},
