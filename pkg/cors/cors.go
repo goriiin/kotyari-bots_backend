76485959722952174045c12b493a399d6005f428
goriiin <afk00.17@bk.ru>
2025-11-13 16:06:33 +0300

    hack-2: rw(?)

--- pkg/cors/cors.go
+++ pkg/cors/cors.go
@@ -19,6 +19,8 @@ func New() *cors.Cors {
 			"http://localhost:8089",
 			"http://83.166.253.130:8089",
 			"http://127.0.0.1:8089",
+
+			"http://212.233.75.64",
 		},
 		AllowCredentials: true,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
