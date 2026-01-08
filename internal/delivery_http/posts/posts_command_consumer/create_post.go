944f565f755c0aec86b5f7236093a33c0d03ae5c
goriiin <afk00.17@bk.ru>
2026-01-08 23:07:40 +0300

    add auth submodule

--- internal/delivery_http/posts/posts_command_consumer/create_post.go
+++ internal/delivery_http/posts/posts_command_consumer/create_post.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/go-faster/errors"
 	"github.com/google/uuid"
@@ -14,62 +15,60 @@ import (
 func (p *PostsCommandConsumer) CreatePost(ctx context.Context, postsMap map[uuid.UUID]model.Post, req posts.KafkaCreatePostRequest) error {
 	postsChan := make(chan model.Post, len(req.Profiles))
 	var wg sync.WaitGroup
+
 	for _, profile := range req.Profiles {
 		wg.Add(1)
-		go func() {
+		go func(prof posts.CreatePostProfiles) {
 			defer wg.Done()
 
-			var (
-				mutex     sync.Mutex
-				profileWg sync.WaitGroup
-			)
+			profileCtx, cancel := context.WithTimeout(ctx, 2*time.Minute)
+			defer cancel()
 
+			var mutex sync.Mutex
+			profileWg := sync.WaitGroup{}
 			profilesPosts := make([]model.Post, 0, 5)
 
-			rewritten, err := p.rewriter.Rewrite(ctx, req.UserPrompt, profile.ProfilePrompt, req.BotPrompt)
+			rewritten, err := p.rewriter.Rewrite(profileCtx, req.UserPrompt, prof.ProfilePrompt, req.BotPrompt)
 			if err != nil {
-				fmt.Println("error rewriting prompts", err)
+				fmt.Println("error rewriting prompts:", err)
 				return
 			}
 
 			for _, rw := range rewritten {
 				profileWg.Add(1)
-				go func() {
+				go func(rewrittenText string) {
 					defer profileWg.Done()
 
-					generatedPostContent, err := p.getter.GetPost(ctx, rw, profile.ProfilePrompt, req.BotPrompt)
+					generatedPostContent, err := p.getter.GetPost(profileCtx, rewrittenText, prof.ProfilePrompt, req.BotPrompt)
 					if err != nil {
-						fmt.Println("error getting post", err)
+						fmt.Println("error getting post:", err)
 						return
 					}
 
-					post := postsMap[profile.ProfileID]
+					post := postsMap[prof.ProfileID]
 					post.Title = generatedPostContent.PostTitle
 					post.Text = generatedPostContent.PostText
 
 					mutex.Lock()
 					profilesPosts = append(profilesPosts, post)
 					mutex.Unlock()
-				}()
+				}(rw)
 			}
 
 			profileWg.Wait()
 
-			bestPostCandidate, err := p.judge.SelectBest(ctx, req.UserPrompt, profile.ProfilePrompt, req.BotPrompt,
-				posts.PostsToCandidates(profilesPosts))
-
+			bestPostCandidate, err := p.judge.SelectBest(profileCtx, req.UserPrompt, prof.ProfilePrompt, req.BotPrompt, posts.PostsToCandidates(profilesPosts))
 			if err != nil {
-				fmt.Println("error getting best post ", err)
+				fmt.Println("error getting best post:", err)
 				return
 			}
 
-			bestPost := postsMap[profile.ProfileID]
-
+			bestPost := postsMap[prof.ProfileID]
 			bestPost.Text = bestPostCandidate.Text
 			bestPost.Title = bestPostCandidate.Title
 
 			postsChan <- bestPost
-		}()
+		}(profile)
 	}
 
 	go func() {
